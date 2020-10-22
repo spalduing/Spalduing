a5ab69889d30804902b7945afecda01dd5109773
Spalduing <spalduing12@gmail.com>
2020-10-22 14:50:43 -0500

    Print_Square project added, queda pendiente hacerlo

---
+++ Print_Square/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int print_square();
+
+int main()
+{
+    print_square();
+    return 0;
+}
+
+/* Draw a square in the matrix */
+int print_square()
+{
+
+    return 0;
+}
