6013a54162e72ca97b792a150dcf1d607619a0f5
Spalduing <spalduing12@gmail.com>
2020-10-22 13:32:57 -0500

    Upper_Diagonal project added

--- Upper_Diagonal/main.cpp
+++ Upper_Diagonal/main.cpp
@@ -2,29 +2,27 @@
 
 using namespace std;
 
-int revInfDiag();
-
+int display_upper_diagonal();
 int main()
 {
-
-
-    revInfDiag();
-
+    display_upper_diagonal();
     return 0;
 }
 
-/* Reverse Inferior diagonal of a matrix */
-int revInfDiag()
+/* Superior diagonal of a matrix */
+int display_upper_diagonal()
 {
+    int m_dimension, n_dimension, number = 0;
+
     //ENTERING THE DIMENSION OF THE ARRAY//
-    int m_dimension, n_dimension, number;
     cout << "Enter the m dimension of the matrix: ";
     cin >> m_dimension;
     cout << "Enter the n dimension of the matrix: ";
     cin >> n_dimension;
 
-    //INITIALIZING THE ARRAY WITH ZEROS//
     int matrix[m_dimension][n_dimension];
+
+    //INITIALIZING THE ARRAY WITH ZEROS//
     for(int i=0; i<m_dimension;i++){
         for(int j=0; j<n_dimension; j++){
             matrix[i][j]=0;
@@ -33,18 +31,20 @@ int revInfDiag()
         cout<<"\n";
     }
 
-    //ENTERING THE NUMBERS IN THE ARRAY//
-    int column_cap = 0;
-    for(int i=m_dimension-1; i>-1; i--){
-        for(int j=column_cap; j<n_dimension; j++){
+    int column_cap = n_dimension;
+
+    //ENTERING THE NUMBERS//
+    for(int i=0; i<m_dimension;i++){
+        for(int j=0; j<column_cap; j++){
             cout << "Enter the number to put in the ["<<i<<"] "<<"["<<j<<"] position: ";
             cin>> number;
-            matrix[i][j]=number;
+            matrix[i][j] = number;
             cout<<"\n";
         }
-        column_cap++;
+        column_cap--;
         cout<<"\n";
     }
+
     for(int k=0; k<m_dimension;k++){
         for(int l=0; l<n_dimension; l++){
             cout << "["<<matrix[k][l]<<"] ";
