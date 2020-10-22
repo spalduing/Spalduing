e2b9033481ae327f5eaf3f5f809a19e64e0cb625
Spalduing <spalduing12@gmail.com>
2020-10-22 10:05:07 -0500

    Histogram project added, it displays a little histograms of a given set of numbers

--- Histogram/main.cpp
+++ Histogram/main.cpp
@@ -1,48 +1,75 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int histogram();
-int histogram2(vector<int> &v,int s);
+int display_histograms();
+int display_histograms2(vector<int> &v,int s);
 
 int main()
 {
+    vector<int> histogram = {3,7,4,5,8,2};
+
+    cout << " FIRST DISPLAY HISTOGRAM METHOD" << endl;
+    cout << " ================================= " << endl;
+
+    display_histograms();
+
+    cout << endl << endl;
+
+    cout << " FIRST DISPLAY HISTOGRAM METHOD" << endl;
+    cout << " ================================= " << endl;
+
+    display_histograms2(histogram, sizeof(histogram)/sizeof(int));
 
     return 0;
 }
 
 
 /* Draw an histogram to the given set of numbers*/
-int histogram()
+int display_histograms()
 {
-    int n[5];
-    int v;
-    for(int j=0; j<5; j++){
-    cout<<"Enter a number: ";
-    cin>>v;
-    n[j]=v;
+    int historgram[5] = {};
+    int number_elements;
+    int index = 0;
+
+    for(int row=0; row<5; row++){
+        cout<<"Enter a number of elements for this row: ";
+
+        cin >> number_elements;
+
+        historgram[row]=number_elements;
     }
+
+    cout << endl;
+
     for(int i=0; i<5;i++){
-            cout<<n[i]<<" ";
-            for(int k=0;k<n[i];k++){
-        cout << "*";
+        index = i;
+        cout<< index + 1 <<": ";
+
+        for(int j=0;j<historgram[i];j++){
+            cout << "*";
         }
-        cout << "\n";
+    cout << "\n";
     }
-        return 0;
+
+    return 0;
 
 }
 
 /* Draw an histogram to the given set of numbers*/
-int histogram2(vector<int> &v,int s)
+int display_histograms2(vector<int> &v,int s)
 {
+    int index;
     for(int i=0; i<s;i++){
-            cout<<i<<": "<< v[i] << " ";
-            for(int k=0;k<v[i];k++){
-        cout << "*";
+        index = i;
+        cout<< index + 1 <<": "<< v[i] << " ";
+
+        for(int k=0;k<v[i];k++){
+            cout << "*";
         }
         cout << "\n";
     }
-        return 0;
 
+    return 0;
 }
